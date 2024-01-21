990717abd7089c22b72b532ab48c2878f98c99c6
ana-sta-si <146162874+ana-sta-si@users.noreply.github.com>
2024-01-21 12:19:23 +0300

    tabels.py

--- tabels.py
+++ tabels.py
@@ -1,9 +1,9 @@
 import tkinter as tk
 from tkinter import ttk
 from datetime import datetime
-from time import sleep
 import sqlite3
 import func_btn
+import os
 #главное окно приложения
 class MainApp(tk.Tk):
     # работа с базой данных
@@ -114,21 +114,7 @@ class Frame_menu(tk.Frame):
         #     elif i == 2 and i == button:
         #         frame = MainApp.frames[Frame_pizza]
         #         frame.tkraise()
-class Frame_zakaz(tk.Frame):
-    def __init__(self, parent):
-        super().__init__(parent)
-        self.place(y=150, x=0, height=605, width=750)
-        self.configure(background="white")
-        # определяем столбцы
-        columns = ("name", "number", "price")
-        tree = ttk.Treeview(self, columns=columns, show="headings")
-        tree.pack(fill=tk.BOTH, expand=1)
-        style = ttk.Style()
-        style.configure("Treeview", font=('Helvetica', 10))
-        MainApp.curs.execute("SELECT Название_блюда, Количество, Цена_блюда FROM Карта_заказа")
-        result = MainApp.curs.fetchall()
-        for row in result:
-            tree.insert('', tk.END, values=row)
+
     #  способ 2   self.updt(tree)
     # def updt(self, tree):
     #     for x in range(100):
@@ -164,70 +150,59 @@ class Frame_drink(tk.Frame):
     def add_possition(self, button):
         for i in range(len(self.buttons)):
             if i == 0 and i == button:
-                result = MainApp.curs.execute(f'insert into Карта_заказа (Название_блюда, Цена_блюда) select Название, Цена from Меню WHERE Код_раздела = 1 and Код_блюда = {i + 1}')
-                if result != []:
-                    query = "Update Карта_заказа set Количество = 1"
-                MainApp.curs.execute(query)
+                code_b = i + 1
+                record_exists = MainApp.curs.execute('''SELECT Код_блюда, Количество FROM Карта_заказа WHERE Код_блюда = ?''',(code_b,)).fetchone()
+                if record_exists:
+                    MainApp.curs.execute('''UPDATE Карта_заказа SET Количество = ?  WHERE Код_блюда = ?''', (record_exists[1] + 1,  code_b))
+                else:
+                    MainApp.curs.execute('''INSERT  INTO  Карта_заказа(Название_блюда, Цена_блюда, Код_блюда) SELECT Название, Цена, Код_блюда FROM Меню WHERE Код_раздела = 1 and Код_блюда = ?''', (code_b,))
+                    query = f"Update Карта_заказа set Количество = 1"
+                    MainApp.curs.execute(query)
                 MainApp.conn.commit()
                 MainApp.curs.fetchall()
                 MainApp.conn.commit()
+                refresh()
+                print(record_exists)
             elif i == 1 and i == button:
-                result = MainApp.curs.execute(f'insert into Карта_заказа (Название_блюда, Цена_блюда) select Название, Цена from Меню WHERE Код_раздела = 1 and Код_блюда = {i + 1}')
-                if result != []:
-                    l=0
-                    query = f"Update Карта_заказа set Количество = {l+1}"
-                #     l+=1
-                # elif result ==[]:
-                #     query = f"Update Карта_заказа set Количество = {l + 1}"
-                #     l+=1
-                MainApp.curs.execute(query)
+                code_b = i + 1
+                record_exists = MainApp.curs.execute(
+                    '''SELECT Код_блюда, Количество FROM Карта_заказа WHERE Код_блюда = ?''', (code_b,)).fetchone()
+                if record_exists:
+                    MainApp.curs.execute('''UPDATE Карта_заказа SET Количество = ?  WHERE Код_блюда = ?''',
+                                         (record_exists[1] + 1, code_b))
+                else:
+                    MainApp.curs.execute(
+                        '''INSERT  INTO  Карта_заказа(Название_блюда, Цена_блюда, Код_блюда) SELECT Название, Цена, Код_блюда FROM Меню WHERE Код_раздела = 1 and Код_блюда = ?''',
+                        (code_b,))
+                    query = f"Update Карта_заказа set Количество = 1"
+                    MainApp.curs.execute(query)
                 MainApp.conn.commit()
                 MainApp.curs.fetchall()
                 MainApp.conn.commit()
-                # MainApp.curs.execute(f'SELECT Название, Цена from Меню WHERE Код_раздела = 1 and Код_блюда = {i+1}')
-                # rows = MainApp.curs.fetchall()
-                # MainApp.curs.executemany("insert into Карта_заказа (Название_блюда, Цена_блюда) values (%s,%s);", rows)
-                # # cursor1.execute('SELECT * FROM имя_таблицы')
-                # data = cursor1.fetchall()
-                # query = 'INSERT INTO имя_таблицы VALUES (?, ?, ...)'
-                # cursor2.executemany(query, data)
-                #
-                # # Сохранение изменений и закрытие соединений
-                # conn2.commit()
-                # conn2.close()
-                # conn1.close()
-                # MainApp.curs.execute( f'insert into Карта_заказа (Название_блюда, Цена_блюда) select Название, Цена from Меню WHERE Код_раздела = 1 and Код_блюда = {i+1}')
-                # MainApp.curs.fetchall()
-                # MainApp.curs.close()
-                # MainApp.curs.execute(f"SELECT Цена FROM Меню WHERE Код_раздела = 1 and Код_блюда = {i+1}")
-                # result = MainApp.curs.fetchall()
-                # MainApp.curs.execute(f"SELECT Название FROM Меню WHERE Код_раздела = 1 and Код_блюда = {i+1}")
-                # res = MainApp.curs.fetchall()
-                # for row in result:
-                #     for field in row:
-                #         price = field
-                # for row in res:
-                #     for fields in row:
-                #         name = fields
-                #
-                # print(lst)
-        # for i in range(len(self.buttons)):
-        #     if i == 0 and i == button:
-        #         frame = MainApp.frames[Frame_drink]
-        #         frame.tkraise()
-        #     elif i == 1 and i == button:
-        #         frame = MainApp.frames[Frame_alc]
-        #         frame.tkraise()
-        #     elif i == 2 and i == button:
-        #         frame = MainApp.frames[Frame_pizza]
-        #         frame.tkraise()
-        #     MainApp.curs.execute(f"SELECT Название, Цена FROM Меню WHERE Код_раздела = '{code_raz}'")
-        #     list_btn = []
-        #     for row in MainApp.curs:
-        #         for field in row:
-        #             list_btn.append(field)
-        #     print(list_btn)
+                refresh()
 
+
+class Frame_zakaz(tk.Frame):
+    # def resturt(self):
+    #     Frame_zakaz.destroy(self)
+    #     Frame_zakaz.create_table(self)
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.place(y=150, x=0, height=605, width=750)
+        self.configure(background="white")
+        self.tabel = self.create_table()
+    def create_table(self):
+        # определяем столбцы
+        columns = ("name", "number", "price")
+        tree = ttk.Treeview(self, columns=columns, show="headings")
+        tree.pack(fill=tk.BOTH, expand=1)
+        style = ttk.Style()
+        style.configure("Treeview", font=('Helvetica', 10))
+        MainApp.curs.execute("SELECT Название_блюда, Количество, Цена_блюда FROM Карта_заказа")
+        result = MainApp.curs.fetchall()
+        for row in result:
+            tree.insert('', tk.END, values=row)
+        self.update()
 #Фрейм 4 - фрейм алкоголя
 class Frame_alc(tk.Frame):
     def __init__(self, parent):
@@ -270,5 +245,8 @@ class Frame_data_zakaz(tk.Frame):
         label.pack(anchor=tk.E, padx=120, pady=36)
         btn_sk = tk.Button(self, text="Скидка", foreground='black', font="Helvetica 14", borderwidth=0, background='white', width=2, height=3)
         btn_sk.place(x=0, y=0, height=605, width=250)
+def refresh():
+    app.destroy()
+    import tabels
 app = MainApp()
 app.mainloop()
